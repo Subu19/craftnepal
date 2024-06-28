3a6c90d1191a7601d5ef7e98a4283ef54d89e4a8
Subu19 <subuacharya19@gmail.com>
2024-06-28 16:01:21 +0545

    update events

--- src/Events.js
+++ src/Events.js
@@ -3,38 +3,30 @@ import endportal from "./assets/images/endframe.png";
 import Countdown from "react-countdown";
 
 const Events = () => {
-  const [open, setopen] = useState(true);
-  return (
-    <>
-      {open ? (
-        <div className="events-box">
-          <div className="event-header">
-            <div className="event-title">END DIMENTION</div>
-            <i
-              class="fa fa-close event-close"
-              onClick={() => setopen(false)}
-            ></i>
-          </div>
-          <div className="event-body">
-            <div className="event-img-box">
-              <img src={endportal} className="event-img"></img>
-            </div>
-            <div className="countdown-containner">
-              <Countdown
-                className="countdown"
-                date={1688616900000}
-                autoStart={true}
-                daysInHours={false}
-              ></Countdown>
-              <div className="countdown-header">Opens In</div>
-            </div>
-          </div>
-        </div>
-      ) : (
-        ""
-      )}
-    </>
-  );
+    const [open, setopen] = useState(false);
+    return (
+        <>
+            {open ? (
+                <div className="events-box">
+                    <div className="event-header">
+                        <div className="event-title">END DIMENTION</div>
+                        <i class="fa fa-close event-close" onClick={() => setopen(false)}></i>
+                    </div>
+                    <div className="event-body">
+                        <div className="event-img-box">
+                            <img src={endportal} className="event-img"></img>
+                        </div>
+                        <div className="countdown-containner">
+                            <Countdown className="countdown" date={1688616900000} autoStart={true} daysInHours={false}></Countdown>
+                            <div className="countdown-header">Opens In</div>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                ""
+            )}
+        </>
+    );
 };
 
 export default Events;
