27ec8341e22daaff4250108328d48f945f8169f4
Subu19 <subuacharya19@gmail.com>
2024-10-02 19:12:46 +0545

    update

--- src/Events.js
+++ src/Events.js
@@ -3,13 +3,13 @@ import endportal from "./assets/images/endframe.png";
 import Countdown from "react-countdown";
 
 const Events = () => {
-    const [open, setopen] = useState(true);
+    const [open, setopen] = useState(false);
     return (
         <>
             {open ? (
                 <div className="events-box">
                     <div className="event-header">
-                        <div className="event-title">END DIMENTION</div>
+                        <div className="event-title">END DIMENSION</div>
                         <i class="fa fa-close event-close" onClick={() => setopen(false)}></i>
                     </div>
                     <div className="event-body">
@@ -17,7 +17,7 @@ const Events = () => {
                             <img src={endportal} className="event-img"></img>
                         </div>
                         <div className="countdown-containner">
-                            <Countdown className="countdown" date={1723875300000} autoStart={true} daysInHours={false}></Countdown>
+                            <Countdown className="countdown" date={1723904100000} autoStart={true} daysInHours={false}></Countdown>
                             <div className="countdown-header">Opens In</div>
                         </div>
                     </div>
