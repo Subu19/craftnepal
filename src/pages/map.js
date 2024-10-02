a56ce55aaefedb1149a4bf77af6db3123faa0810
Subu19 <subuacharya19@gmail.com>
2024-10-02 20:22:03 +0545

    update urls

--- src/pages/map.js
+++ src/pages/map.js
@@ -3,28 +3,24 @@ import Coffee from "../components/extra/Coffee/Coffee";
 import Nav from "../components/Nav/Nav";
 
 const Map = () => {
-  const [maperror, setmaperror] = useState(false);
-  return (
-    <div className="map">
-      <Coffee></Coffee>
-      <Nav selected="map" focused={true}></Nav>
-      {maperror == false ? (
-        <iframe
-          className="mapContainner"
-          src="https://mapproxy.craftnepal.net"
-          onError={() => setmaperror(true)}
-        ></iframe>
-      ) : (
-        <div className="contentTitle whitetext">
-          Error showing map
-          <div className="normaltext">
-            please fall back to
-            <a href="http://play.craftnepal.net:8169/">Original MAP</a>{" "}
-          </div>
+    const [maperror, setmaperror] = useState(false);
+    return (
+        <div className="map">
+            <Coffee></Coffee>
+            <Nav selected="map" focused={true}></Nav>
+            {maperror == false ? (
+                <iframe className="mapContainner" src="https://map.craftnepal.net" onError={() => setmaperror(true)}></iframe>
+            ) : (
+                <div className="contentTitle whitetext">
+                    Error showing map
+                    <div className="normaltext">
+                        please fall back to
+                        <a href="http://play.craftnepal.net:8169/">Original MAP</a>{" "}
+                    </div>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Map;
