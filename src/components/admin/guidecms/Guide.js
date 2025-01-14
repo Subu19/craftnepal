553127e119393bca9769a700e74d4731e55fef6f
Subu19 <subuacharya19@gmail.com>
2025-01-14 11:35:00 +0545

    fix errors for build

--- src/components/admin/guidecms/Guide.js
+++ src/components/admin/guidecms/Guide.js
@@ -8,84 +8,75 @@ import config from "../../../config.json";
 import RawFormat from "./RawFormat";
 import DropDowns from "../../extra/dropdown/DropDown";
 const Guide = () => {
-  const [user, loading] = useContext(UserContext);
-  const [selection, setSelection] = useState(null);
-  const [selected, setSelected] = useState(null);
-  const [data, setData] = useState(null);
-  const [newData, setNewData] = useState(null);
-  useEffect(() => {
-    axios
-      .get(config.baseUrl + "api/guide")
-      .then((res) => {
-        setSelection(res.data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-  useEffect(() => {
-    if (selected) {
-      setData(null);
-      axios
-        .get(config.baseUrl + "api/guide/" + selected)
-        .then((res) => {
-          setData(res.data);
-        })
-        .catch(() => console.log(err));
-    }
-  }, [selected]);
+    const [user, loading] = useContext(UserContext);
+    const [selection, setSelection] = useState(null);
+    const [selected, setSelected] = useState(null);
+    const [data, setData] = useState(null);
+    const [newData, setNewData] = useState(null);
+    useEffect(() => {
+        axios
+            .get(config.baseUrl + "api/guide")
+            .then((res) => {
+                setSelection(res.data);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+    useEffect(() => {
+        if (selected) {
+            setData(null);
+            axios
+                .get(config.baseUrl + "api/guide/" + selected)
+                .then((res) => {
+                    setData(res.data);
+                })
+                .catch((err) => console.log(err));
+        }
+    }, [selected]);
 
-  return (
-    <>
-      {loading ? (
-        ""
-      ) : user.isAdmin ? (
-        <div className="a-Containner GuideCMS">
-          <div className="a-Selection">
-            <Link to={"/"} className="adminheader">
-              <img src={logo} className="adminLogo"></img>
-              GuideCMS
-            </Link>
-
-            <div className="a-selectionContainner">
-              {selection
-                ? selection.map((guide) => {
-                    return (
-                      <div
-                        className="a-selection"
-                        onClick={() => setSelected(guide.id)}
-                      >
-                        {guide.id}
-                      </div>
-                    );
-                  })
-                : ""}
-            </div>
-          </div>
-
-          <div className="a-Collection">
-            <div className="a-guideRaw">
-              {data ? (
-                <RawFormat data={data} setNewData={setNewData}></RawFormat>
-              ) : (
+    return (
+        <>
+            {loading ? (
                 ""
-              )}
-            </div>
-            <div className="a-guidePreview">
-              {newData ? (
-                <>
-                  <p className="normaltext guideText">{newData.header}</p>
-                  <DropDowns data={newData.data}></DropDowns>
-                </>
-              ) : (
-                ""
-              )}
-            </div>
-          </div>
-        </div>
-      ) : (
-        "You are not admin"
-      )}
-    </>
-  );
+            ) : user.isAdmin ? (
+                <div className="a-Containner GuideCMS">
+                    <div className="a-Selection">
+                        <Link to={"/"} className="adminheader">
+                            <img src={logo} className="adminLogo"></img>
+                            GuideCMS
+                        </Link>
+
+                        <div className="a-selectionContainner">
+                            {selection
+                                ? selection.map((guide) => {
+                                      return (
+                                          <div className="a-selection" onClick={() => setSelected(guide.id)}>
+                                              {guide.id}
+                                          </div>
+                                      );
+                                  })
+                                : ""}
+                        </div>
+                    </div>
+
+                    <div className="a-Collection">
+                        <div className="a-guideRaw">{data ? <RawFormat data={data} setNewData={setNewData}></RawFormat> : ""}</div>
+                        <div className="a-guidePreview">
+                            {newData ? (
+                                <>
+                                    <p className="normaltext guideText">{newData.header}</p>
+                                    <DropDowns data={newData.data}></DropDowns>
+                                </>
+                            ) : (
+                                ""
+                            )}
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                "You are not admin"
+            )}
+        </>
+    );
 };
 
 export default Guide;
--- src/components/feed/Post.js
+++ src/components/feed/Post.js
@@ -8,252 +8,220 @@ import { UserContext } from "../../providers/UserProvider";
 import { useSocket } from "../../providers/SocketProvider";
 import Comment from "./Comment";
 const Post = ({ mainPost, setOpenComment, setPostId, setPosting }) => {
-  const [post, setPost] = useState(mainPost);
-  const socket = useSocket();
-  const commentRef = useRef();
-  const [user, loading] = useContext(UserContext);
-  const [liked, setLiked] = useState(false);
+    const [post, setPost] = useState(mainPost);
+    const socket = useSocket();
+    const commentRef = useRef();
+    const [user, loading] = useContext(UserContext);
+    const [liked, setLiked] = useState(false);
 
-  useEffect(() => {
-    if (post.likes.length > 0) {
-      post.likes.find((like) => like.userId == user.id)
-        ? setLiked(true)
-        : setLiked(false);
-    }
-  }, []);
-  useEffect(() => {
-    if (socket) {
-      socket.on("updatePost:" + post._id, (data) => {
-        setPost(data.post);
-      });
-    }
-  }, [socket]);
-  const likePost = () => {
-    if (!liked)
-      axios
-        .post(config.baseUrl + config.api + "post/like", { postId: post._id })
-        .then((res) => {
-          if (res.data.err) {
-            alert("error while liking!");
-          } else {
-            setLiked(true);
-            if (socket)
-              socket.emit("updatePost", {
-                postId: post._id,
-              });
-          }
-        })
-        .catch((err) => console.log(err));
-    else
-      axios
-        .post(config.baseUrl + config.api + "post/unlike", { postId: post._id })
-        .then((res) => {
-          if (res.data.err) {
-            alert("error while Unliking!");
-          } else {
-            setLiked(false);
-            if (socket)
-              socket.emit("updatePost", {
-                postId: post._id,
-              });
-          }
-        });
-  };
-  const submitComment = (e) => {
-    e.preventDefault();
-    if (commentRef.current.value.trim() !== "") {
-      axios
-        .post(config.baseUrl + config.api + "post/comment", {
-          postId: post._id,
-          comment: commentRef.current.value.trim(),
-        })
-        .then((res) => {
-          if (res.data.err) {
-            alert("error while commenting!");
-          } else {
-            commentRef.current.value = "";
-            if (socket)
-              socket.emit("updatePost", {
-                postId: post._id,
-              });
-          }
-        });
-    }
-  };
-  const getTime = (postTime) => {
-    var time = Date.now() - postTime;
-    let seconds = Math.floor(time / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
-    let days = Math.floor(hours / 24);
-    seconds = seconds % 60;
-    minutes = minutes % 60;
-    if (minutes < 1) {
-      return seconds + " sec ago";
-    } else if (hours < 1) {
-      return minutes + " min ago";
-    } else if (days < 1) {
-      return hours + " hr ago";
-    } else {
-      return days + " days ago";
-    }
-  };
+    useEffect(() => {
+        if (post.likes.length > 0) {
+            post.likes.find((like) => like.userId == user.id) ? setLiked(true) : setLiked(false);
+        }
+    }, []);
+    useEffect(() => {
+        if (socket) {
+            socket.on("updatePost:" + post._id, (data) => {
+                setPost(data.post);
+            });
+        }
+    }, [socket]);
+    const likePost = () => {
+        if (!liked)
+            axios
+                .post(config.baseUrl + config.api + "post/like", { postId: post._id })
+                .then((res) => {
+                    if (res.data.err) {
+                        alert("error while liking!");
+                    } else {
+                        setLiked(true);
+                        if (socket)
+                            socket.emit("updatePost", {
+                                postId: post._id,
+                            });
+                    }
+                })
+                .catch((err) => console.log(err));
+        else
+            axios.post(config.baseUrl + config.api + "post/unlike", { postId: post._id }).then((res) => {
+                if (res.data.err) {
+                    alert("error while Unliking!");
+                } else {
+                    setLiked(false);
+                    if (socket)
+                        socket.emit("updatePost", {
+                            postId: post._id,
+                        });
+                }
+            });
+    };
+    const submitComment = (e) => {
+        e.preventDefault();
+        if (commentRef.current.value.trim() !== "") {
+            axios
+                .post(config.baseUrl + config.api + "post/comment", {
+                    postId: post._id,
+                    comment: commentRef.current.value.trim(),
+                })
+                .then((res) => {
+                    if (res.data.err) {
+                        alert("error while commenting!");
+                    } else {
+                        commentRef.current.value = "";
+                        if (socket)
+                            socket.emit("updatePost", {
+                                postId: post._id,
+                            });
+                    }
+                });
+        }
+    };
+    const getTime = (postTime) => {
+        var time = Date.now() - postTime;
+        let seconds = Math.floor(time / 1000);
+        let minutes = Math.floor(seconds / 60);
+        let hours = Math.floor(minutes / 60);
+        let days = Math.floor(hours / 24);
+        seconds = seconds % 60;
+        minutes = minutes % 60;
+        if (minutes < 1) {
+            return seconds + " sec ago";
+        } else if (hours < 1) {
+            return minutes + " min ago";
+        } else if (days < 1) {
+            return hours + " hr ago";
+        } else {
+            return days + " days ago";
+        }
+    };
 
-  return (
-    <div className="post">
-      {post.author.id == user.id || user.isAdmin ? (
-        <>
-          <i class="fa fa-ellipsis-v optionIcon"></i>
-          <Options setPosting={setPosting} postId={post._id}></Options>
-        </>
-      ) : (
-        ""
-      )}
+    return (
+        <div className="post">
+            {post.author.id == user.id || user.isAdmin ? (
+                <>
+                    <i class="fa fa-ellipsis-v optionIcon"></i>
+                    <Options setPosting={setPosting} postId={post._id}></Options>
+                </>
+            ) : (
+                ""
+            )}
 
-      <div className="postHeader">
-        <img
-          className="userImage"
-          src={config.avatar + post.author.id + "/" + post.author.profilePic}
-        ></img>
-        <b className="username">{post.author.username}</b>
-        <i className="postDate">{getTime(post.id)}</i>
-      </div>
-      <div className="postCaption">{post.caption}</div>
-      <img
-        className="postImage"
-        src={config.baseUrl + "uploads/" + post.postImage}
-      ></img>
-      <div className="postFooter">
-        <div className={user.id ? "footerIcons" : "footerIcons blockSign"}>
-          <div className="likes">
-            <img
-              className={
-                (liked ? "heartIcon" : "unheartIcon") +
-                (user.id ? "" : " disableUser")
-              }
-              src={heartIcon}
-              onClick={() => likePost()}
-            ></img>
-            <b className="footerTitle">{post.likes.length}</b>
-          </div>
-          <div
-            className={user.id ? "comments " : "disableUser comments"}
-            onClick={() => {
-              setOpenComment(true);
-              setPostId(post._id);
-            }}
-          >
-            <i class="material-icons commentIcon">chat</i>
-            <b className="footerTitle">{post.comments.length}</b>
-          </div>
+            <div className="postHeader">
+                <img className="userImage" src={config.avatar + post.author.id + "/" + post.author.profilePic}></img>
+                <b className="username">{post.author.username}</b>
+                <i className="postDate">{getTime(post.id)}</i>
+            </div>
+            <div className="postCaption">{post.caption}</div>
+            <img className="postImage" src={config.baseUrl + "uploads/" + post.postImage}></img>
+            <div className="postFooter">
+                <div className={user.id ? "footerIcons" : "footerIcons blockSign"}>
+                    <div className="likes">
+                        <img
+                            className={(liked ? "heartIcon" : "unheartIcon") + (user.id ? "" : " disableUser")}
+                            src={heartIcon}
+                            onClick={() => likePost()}
+                        ></img>
+                        <b className="footerTitle">{post.likes.length}</b>
+                    </div>
+                    <div
+                        className={user.id ? "comments " : "disableUser comments"}
+                        onClick={() => {
+                            setOpenComment(true);
+                            setPostId(post._id);
+                        }}
+                    >
+                        <i class="material-icons commentIcon">chat</i>
+                        <b className="footerTitle">{post.comments.length}</b>
+                    </div>
+                </div>
+                {user.id ? (
+                    <form className="comment" onSubmit={(e) => submitComment(e)}>
+                        <input className="commentInput" aria-autocomplete="false" placeholder="Reply with something.." ref={commentRef}></input>
+                        <button type="submit" className="sendButton">
+                            <i class="material-icons sendIcon">send</i>
+                        </button>
+                    </form>
+                ) : (
+                    ""
+                )}
+            </div>
         </div>
-        {user.id ? (
-          <form className="comment" onSubmit={(e) => submitComment(e)}>
-            <input
-              className="commentInput"
-              aria-autocomplete="false"
-              placeholder="Reply with something.."
-              ref={commentRef}
-            ></input>
-            <button type="submit" className="sendButton">
-              <i class="material-icons sendIcon">send</i>
-            </button>
-          </form>
-        ) : (
-          ""
-        )}
-      </div>
-    </div>
-  );
+    );
 };
 
 const Options = ({ postId, setPosting }) => {
-  const [show, setShow] = useState(false);
-  const deletePost = () => {
-    const c = confirm("Are you sure you want to delete this post?");
-    if (c) {
-      setPosting(true);
-      axios
-        .post(config.baseUrl + config.api + "post/delete/" + postId)
-        .then((res) => {
-          setPosting(false);
-        })
-        .catch((err) => {
-          alert("Error while deleting!");
-        });
-    }
-    setShow(false);
-  };
-  return (
-    <>
-      <i class="fa fa-ellipsis-v optionIcon" onClick={() => setShow(!show)}></i>
-      {show ? (
-        <div className="optionContainner">
-          <div className="options">
-            <div className="option" onClick={() => deletePost()}>
-              <i class="material-icons">delete_sweep</i> Delete
-            </div>
+    const [show, setShow] = useState(false);
+    const deletePost = () => {
+        // eslint-disable-next-line no-restricted-globals
+        const c = confirm("Are you sure you want to delete this post?");
+        if (c) {
+            setPosting(true);
+            axios
+                .post(config.baseUrl + config.api + "post/delete/" + postId)
+                .then((res) => {
+                    setPosting(false);
+                })
+                .catch((err) => {
+                    alert("Error while deleting!");
+                });
+        }
+        setShow(false);
+    };
+    return (
+        <>
+            <i class="fa fa-ellipsis-v optionIcon" onClick={() => setShow(!show)}></i>
+            {show ? (
+                <div className="optionContainner">
+                    <div className="options">
+                        <div className="option" onClick={() => deletePost()}>
+                            <i class="material-icons">delete_sweep</i> Delete
+                        </div>
 
-            <i class="fa fa-sort-up optionPointer"></i>
-          </div>
-        </div>
-      ) : (
-        ""
-      )}
-    </>
-  );
+                        <i class="fa fa-sort-up optionPointer"></i>
+                    </div>
+                </div>
+            ) : (
+                ""
+            )}
+        </>
+    );
 };
 export const Posts = ({ posting, setPosting }) => {
-  const { feed, loading, limit, setLimit, loadingMore } = useFetchFeed(posting);
-  const [openComment, setOpenComment] = useState(false);
-  const [postId, setPostId] = useState();
+    const { feed, loading, limit, setLimit, loadingMore } = useFetchFeed(posting);
+    const [openComment, setOpenComment] = useState(false);
+    const [postId, setPostId] = useState();
 
-  return (
-    <>
-      {loading ? (
-        <div className="loadingContainner">
-          <CreeperLoading></CreeperLoading>
-        </div>
-      ) : feed.err ? (
-        <div className="contentTitle">Something went wrong!</div>
-      ) : feed ? (
-        feed.map((post) => (
-          <Post
-            setPosting={setPosting}
-            mainPost={post}
-            setOpenComment={setOpenComment}
-            setPostId={setPostId}
-          ></Post>
-        ))
-      ) : (
-        ""
-      )}
-      {!loadingMore ? (
-        <button
-          className="showMore"
-          onClick={() => {
-            setLimit(limit + 10);
-          }}
-        >
-          Show More
-        </button>
-      ) : !loading ? (
-        <i class="fa fa-circle-o-notch fa-spin loadingCircle"></i>
-      ) : (
-        ""
-      )}
+    return (
+        <>
+            {loading ? (
+                <div className="loadingContainner">
+                    <CreeperLoading></CreeperLoading>
+                </div>
+            ) : feed.err ? (
+                <div className="contentTitle">Something went wrong!</div>
+            ) : feed ? (
+                feed.map((post) => <Post setPosting={setPosting} mainPost={post} setOpenComment={setOpenComment} setPostId={setPostId}></Post>)
+            ) : (
+                ""
+            )}
+            {!loadingMore ? (
+                <button
+                    className="showMore"
+                    onClick={() => {
+                        setLimit(limit + 10);
+                    }}
+                >
+                    Show More
+                </button>
+            ) : !loading ? (
+                <i class="fa fa-circle-o-notch fa-spin loadingCircle"></i>
+            ) : (
+                ""
+            )}
 
-      {openComment ? (
-        <Comment
-          setOpenComment={setOpenComment}
-          openComment={openComment}
-          postId={postId}
-        ></Comment>
-      ) : (
-        ""
-      )}
-    </>
-  );
+            {openComment ? <Comment setOpenComment={setOpenComment} openComment={openComment} postId={postId}></Comment> : ""}
+        </>
+    );
 };
 
 export default Post;
