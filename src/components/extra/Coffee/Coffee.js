d85f87221bd8d57b5924a033620c83aaf5a8a749
Subu19 <subuacharya19@gmail.com>
2025-01-19 12:04:47 +0545

    update coffee

--- src/components/extra/Coffee/Coffee.js
+++ src/components/extra/Coffee/Coffee.js
@@ -1,26 +1,26 @@
 import React, { useEffect, useState } from "react";
 import "./main.css";
 const Coffee = () => {
-  const [show, toggle] = useState(false);
-  useEffect(() => {
-    if (show) {
-    } else {
-    }
-  }, [show]);
-  return (
-    <div className={show ? "CoffeeContainner coffeeShow" : "CoffeeContainner"}>
-      <i class="fa fa-coffee coffee" onClick={() => toggle(!show)}></i>
-      <div className="coffeeHeader">
-        <b className="normaltext coffeeText ">Chya Kham na {":)"}</b>
-      </div>
-      <div className="coffContent">
-        <img
-          className="supportImage"
-          src="https://media.discordapp.net/attachments/725600128852820039/853620336716283934/IMG_20210613_184625.jpg?width=523&height=663"
-        ></img>
-      </div>
-    </div>
-  );
+    const [show, toggle] = useState(false);
+    useEffect(() => {
+        if (show) {
+        } else {
+        }
+    }, [show]);
+    return (
+        <div className={show ? "CoffeeContainner coffeeShow" : "CoffeeContainner"}>
+            <i class="fa fa-coffee coffee" onClick={() => toggle(!show)}></i>
+            <div className="coffeeHeader">
+                <b className="normaltext coffeeText ">Chya Kham na {":)"}</b>
+            </div>
+            <div className="coffContent">
+                <img
+                    className="supportImage"
+                    src="https://cdn.discordapp.com/attachments/1135250278401187951/1135256475883815062/image.png?ex=678dd93b&is=678c87bb&hm=f83b25afaaa2acad24a31a7dc3ec672c1a34d192019939a8ae8b9afaa2a2bb19&"
+                ></img>
+            </div>
+        </div>
+    );
 };
 
 export default Coffee;
